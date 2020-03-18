3d2ac27b980dc4ea9576cd2cf995def6d2037b5b
Camilo Hurtado <chc201627@gmail.com>
2020-03-18 16:48:55 -0500

    simbolos de comparacion

---
+++ operadorescomparacion.js
@@ -0,0 +1,14 @@
+const numero1 = 20
+const numero2 = 50
+const numero3 = '20'
+console.log(numero1 > numero2);
+console.log(numero1 < numero2);
+
+// Comprador de igual
+console.log(20 === "20");
+
+// Diferentes
+console.log(2 != 3);
+
+console.log('hola'=='hola');
+console.log('a' > 'b');
