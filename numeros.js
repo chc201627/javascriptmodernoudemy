94491c23f3dc7f256f09164ab86fab4cd307885a
Camilo Hurtado <chc201627@gmail.com>
2020-03-18 16:19:17 -0500

    Operaciones numeros javascript

---
+++ numeros.js
@@ -0,0 +1,41 @@
+const numero1= 30
+const numero2 = 20
+const numero3 = 20.20
+const numero4 = .1020
+const numero5 = -3
+
+let resultado;
+//suma
+resultado = numero1 + numero2;
+//resta
+resultado = numero1 - numero2
+//Muliplicar
+resultado = numero2 * 2;
+//divisiones 
+resultado = numero1 / numero2;
+//Modulo 
+resultado = numero1 % numero2;
+//Pi
+resultado = Math.PI
+//redondeo
+resultado = Math.round(2.5)
+//redondeo  hacia arriba
+resultado = Math.ceil(2.1)
+//redondeo hacia abajo
+resultado = Math.floor(2.9)
+// raiz cuadrada
+resultado = Math.sqrt(144)
+// absoluto
+resultado = Math.abs(numero5)
+//potencia 
+resultado = Math.pow(8,3);
+//Minimo
+resultado = Math.min (3,5,9,7,8);
+//Maximo
+resultado = Math.max(3,6,5,9,7,8);
+//Aleatorio
+resultado = Math.random();
+
+resultado = 10 + 20 * 5;
+
+console.log(resultado)
