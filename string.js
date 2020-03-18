6319322d4063e917fc7a0102634c14dfe82ad826
Camilo Hurtado <chc201627@gmail.com>
2020-03-18 15:57:21 -0500

    Operaciones string

---
+++ string.js
@@ -0,0 +1,16 @@
+let aprendiendo = 'Aprendiendo',
+    tecnologia = 'JavaScript';
+
+    let mensaje = 'Aprendiendo Javascript, CSS, HTML para frontend';
+    // COncatenar
+    console.log(tecnologia.concat(' ','Es genial'));
+// Pasar string a mayusculas
+    console.log(tecnologia.toLocaleUpperCase());
+
+    console.log(mensaje.indexOf('HTML'));
+
+    console.log(mensaje.substring(0,13));
+
+    console.log(mensaje.split(' '));
+
+    console.log(mensaje.replace('CSS','PHP'));
---
+++ variables.js
@@ -0,0 +1,25 @@
+// var 
+// var nombre = 'Camilo',
+//     carrito = 'Libro 2';
+
+
+// console.log(nombre);
+// console.log(carrito);
+
+// No se permiten caracteres especiales al inicio
+
+// Mas de un palabra
+
+var primerNombre= 'Camilo'; //camelcase
+var primer_nombre = 'Camilo' // underscore
+var PrimerNombre = 'Juan'; //pascalcase
+
+// let
+let nombre = 'Camilo';
+let nombre1 = 'Camilo';
+console.log(nombre);
+
+//const 
+const producto='Libro !';
+
+console.log(producto);
