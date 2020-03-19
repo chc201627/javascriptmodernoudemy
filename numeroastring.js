701ccd8cdc7bbc452c8f132164079f98e635bf16
Camilo Hurtado <chc201627@gmail.com>
2020-03-19 15:10:25 -0500

    numeros a string

---
+++ numeroastring.js
@@ -0,0 +1,17 @@
+const nombre = 'Juan';
+let cp;
+cp = 90210;
+cp = String(cp)
+console.log(cp.length);
+
+let dato;
+dato = '4' + '4';
+console.log(dato.length);
+
+//De arreglo a String
+
+dato = [1,2,3].toString();
+
+console.log(dato);
+console.log(dato.length);
+console.log(typeof dato);
---
+++ stringanumeros.js
@@ -0,0 +1,25 @@
+const numero1 = "50",
+        numero2 = 10,
+        numero3 = 'tres';
+
+        // console.log(numero1 + numero2);
+        //Conversiones
+console.log(Number(numero1) + numero2);
+console.log(parseInt(numero1) + numero2);
+
+
+let dato;
+    dato = Number("20");
+    dato = Number('20.10391');
+    dato = Number(true);
+    dato = Number(false);
+    
+// ParseFloat y parseInt
+
+dato = parseInt('100');
+dato = parseFloat('100.12');
+dato = parseInt('110.55')
+//tFixed
+dato2 = 189765.555385747899
+console.log(parseFloat(dato2).toFixed(5));
+    console.log(dato);
