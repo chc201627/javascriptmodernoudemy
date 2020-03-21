97508bd46e2338fb08ee858d5b74e7075a1008cb
Camilo Hurtado <chc201627@gmail.com>
2020-03-21 11:10:39 -0500

    queryselectorall

---
+++ dom/universidad/js/byclassname.js
@@ -0,0 +1,7 @@
+let enlace = document.getElementsByClassName('enlace');
+enlace = enlace[3]
+enlace.style.background = '#000'
+
+// mezclar queryselecto y classname
+const listaEnlace = document.querySelector('')
+console.log(enlace);
---
+++ dom/universidad/js/queryselector.js
@@ -0,0 +1,5 @@
+//Query Selector
+const encabezado = document.querySelector('#encabezado');
+encabezado.style.background='#000'
+encabezado.textContent = 'Camilo'
+console.log(encabezado);
