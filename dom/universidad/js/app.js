904fa880a10535a9d5088e2c189c858e235df802
Camilo Hurtado <chc201627@gmail.com>
2020-03-21 11:15:41 -0500

    children js

--- dom/universidad/js/app.js
+++ dom/universidad/js/app.js
@@ -1,3 +1,10 @@
-let enlaces = document.querySelectorAll('#principal .enlace');
-enlaces[1].style.background = '#000';
-console.log(enlaces);
+const navegacion  = document.querySelector('#principal')
+
+console.log(navegacion.children[0].nodeType);
+
+// 1 = elementos
+// 2 = atributos
+//3 = text  node
+//8 = comentarios
+//9 = documentos
+//10 - doctype
